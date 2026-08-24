8a105d304d9015d67f29fe81833f62e6fdfe43f0
Emerson-Martinez-0516 <emartine895@gmail.com>
2026-08-23 20:42:25 -0600

    Creación del arreglo Mascota

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,18 @@
 public class Main {
     public static void main(String[] args){
 
-        Mascota ave1 = new Ave("Chato",
-                "verde",
-                18,
-                "Exotica",
-                1,
-                "MA01");
+        Mascota perro1 = new Perro("Rocky", "Café", 24, "Labrador", 30, "M002");
+        Mascota gato1 = new Gato("Pepe", "Gris con blanco", 1, "Felino", 3, true, "M001");
+        Mascota ave1 = new Ave("Pepito", "Verde", 18, "Exotica", 1, "M003");
 
+        Mascota[] mascotas = { perro1, gato1, ave1 };
 
-        Mascota gato1 = new Gato(
-                "Pepe",
-                "Gris con blanco",
-                1,
-                "Felino",
-                3,
-                true,
-                "MGO1");
-
-        Mascota perro1 = new Perro(
-                "Canela",
-                "Cafe claro",
-                22,
-                "Canino",
-                3,
-                "MP01"
-        );
-
-        ave1.emitirSonido();
-        gato1.emitirSonido();
-        perro1.emitirSonido();
-
-
+         for (Mascota m : mascotas) {
+            m.emitirSonido();
+            System.out.println("Info: " + m.mostrarInformacion());
+            System.out.println("Costo de consulta: Q" + m.calcularCostoConsulta());
+            System.out.println("-------------------------");
+        }
 
     }
 }
